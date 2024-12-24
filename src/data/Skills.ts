4588b0d0d3f6fea09c5248daf880c0f3d994f845
Paul Guachichullca <paulguachichullca023@gmail.com>
2024-12-24 10:04:45 -0500

    Organizando directorios internos

---
+++ src/data/Skills.ts
@@ -0,0 +1,19 @@
+interface Skill {
+  name: string;
+  image: string;
+}
+
+export const skills: Skill[] = [
+  { name: "HTML", image: "/images/logos/html.png" },
+  { name: "CSS", image: "/images/logos/css.png" },
+  { name: "JavaScript", image: "/images/logos/js.png" },
+  { name: "Astro", image: "/images/logos/astro.png" },
+  { name: "TypeScript", image: "/images/logos/ts.png" },
+  { name: "Tailwind CSS", image: "/images/logos/tailwind.png" },
+  { name: "React", image: "/images/logos/react.png" },
+  { name: "GitHub", image: "/images/logos/github.png" },
+  { name: "Sass", image: "/images/logos/sass.png" },
+  { name: "Vite", image: "/images/logos/vite.png" },
+  { name: "VSCode", image: "/images/logos/vscode.png" },
+  { name: "Figma", image: "/images/logos/figma.png" },
+];
